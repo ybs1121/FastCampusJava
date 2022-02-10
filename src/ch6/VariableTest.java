3ec3890f029179c5c8a1d0f7fb8aad6fc14320b1
YangByoungSeok <ybs991@naver.com>
2022-02-10 15:19:10 +0900

    Initial commit

---
+++ src/ch6/VariableTest.java
@@ -0,0 +1,15 @@
+package ch6;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte bnum = 127;
+        System.out.println(bnum);
+
+        int num = 2147383648;
+        System.out.println(num);
+
+        long lNum = 12312312312L;
+        System.out.println(lNum);
+
+    }
+}
