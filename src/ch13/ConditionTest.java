542052a10480e59958341049df7bd2c264a4bea4
YangByoungSeok <ybs991@naver.com>
2022-02-14 12:41:51 +0900

    패스트 캠퍼스 자바 기초 ch13

---
+++ src/ch13/ConditionTest.java
@@ -0,0 +1,18 @@
+package ch13;
+
+import java.util.Scanner;
+
+public class ConditionTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
+        int max = (num1 > num2) ? num1:num2;
+        System.out.println(max);
+
+
+
+    }
+}
